8a491ce478db704b3db8ffbf9f0523f6ec9026fc
cjrocha <Marateodora2005>
2021-12-10 20:34:07 +0200

    Start from new

---
+++ src/com/unpontdev/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.unpontdev;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hi World !\nThis is Java saying\nHello!");
+    }
+
+}
